e7c46bb054faebb4fe2235530b09755622b2cb88
Alexandra Nastase <alexandra@alexandras-mbp.home>
2026-07-01 22:35:58 +0100

    Rename medications table file and add new SQL scripts

--- medications-table.sql
+++ medications-table.sql
@@ -5,6 +5,6 @@ USE NHS_Trust_DB;
 CREATE TABLE Medications (
     MedicationID BIGINT NOT NULL AUTO_INCREMENT,
     MedicationName VARCHAR(150) NOT NULL,
-    Description TEXT,
+    Descritption TEXT,
     PRIMARY KEY (MedicationID)
 );
