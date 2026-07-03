df7396e7cdbcde9092f05e1d5bfd02e27f7bff61
AlexandraNas <alexandranastase978@gmail.com>
2026-07-03 01:21:37 +0100

    Delete queries/aggregate-functions.sql

--- queries/aggregate-functions.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    c.ClinicName,
-    COUNT(a.AppointmentID) AS TotalAppointments,
-    SUM(CASE WHEN a.Status = 'Completed' THEN 1 ELSE 0 END) AS CompletedAppointments,
-    ROUND((SUM(CASE WHEN a.Status = 'Completed' THEN 1 ELSE 0 END) / COUNT(a.AppointmentID)) * 100, 2) AS CompletionRatePercentage
-FROM Clinics c
-LEFT JOIN Appointments a ON c.ClinicID = a.ClinicID
-GROUP BY c.ClinicID, c.ClinicName;
