5d83852f82c4dae82564ce2d1f7478fb32f4cd18
AlexandraNas <alexandranastase978@gmail.com>
2026-07-03 01:21:28 +0100

    Delete queries/dense-rank.sql

--- queries/dense-rank.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    d.Speciality,
-    CONCAT(d.DoctorName, ' ', d.DoctorSurname) AS DoctorName,
-    COUNT(a.AppointmentID) AS TotalAppointments,
-    DENSE_RANK() OVER (PARTITION BY d.Speciality ORDER BY COUNT(a.AppointmentID) DESC) AS DoctorRankWithinSpecialty
-FROM Doctors d
-LEFT JOIN Appointments a ON d.DoctorID = a.DoctorID
-GROUP BY d.DoctorID, d.Speciality, d.DoctorName, d.DoctorSurname;
